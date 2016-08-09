8fd319d70cac6a7fe51acea1b493fe9278864996
Jesse Hull <jessehull@gmail.com>
2016-08-09 12:00:25 -0700

    increment version number.

--- src/dashbot.js
+++ src/dashbot.js
@@ -5,7 +5,7 @@ var uuid = require('node-uuid');
 var _ = require('lodash');
 const util = require('util');
 
-var VERSION = '0.7.0';
+var VERSION = '0.7.1';
 
 function DashBotFacebook(apiKey, urlRoot, debug) {
   var that = this;
