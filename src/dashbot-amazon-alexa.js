75f1711e2a59a4bc6cf2b01eac7ffe21c2f7b1b0
Ito Zapata <ito@dashbot.io>
2017-05-08 13:51:01 -0700

    Alexa and Facebook update
    - easier Alexa integration
    - event logging added to Facebook client library in preparation for extensions.
    - updated the facebook example to include it.

--- src/dashbot-amazon-alexa.js
+++ src/dashbot-amazon-alexa.js
@@ -1,7 +1,8 @@
 'use strict'
 
 var _ = require('lodash');
-var makeRequest = require('./make-request')
+var makeRequest = require('./make-request');
+var meld = require('meld');
 
 var VERSION = require('../package.json').version;
 
@@ -64,6 +65,59 @@ function DashBotAmazonAlexa(apiKey, urlRoot, debug, printErrors) {
     };
     return internalLogOutgoing(data, 'npm');
   };
+
+  const setupContextAspect = function(context, event, logIncoming) {
+    // send off any successes that used the context to respond
+    meld.around(context, 'succeed', function(joinpoint) {
+      const responseBody = joinpoint.args[0];
+      const logOutgoing = that.logOutgoing(event, responseBody);
+
+      // wait for everything to be sent off to dashbot before proceeding due to lambda
+      // not allowing the event loop to drain before terminating execution.
+      Promise.all([logIncoming, logOutgoing]).then(function() {
+        joinpoint.proceed();
+      }).catch(function(errors) {
+        if(that.printErrors) {
+          console.error(errors);
+        }
+        // otherwise do nothing and continue with their code.
+      })
+    })
+
+    // send off any failures that used the context to respond
+    meld.around(context, 'fail', function(joinpoint) {
+      Promise.all([logIncoming]).then(function() {
+        joinpoint.proceed();
+      }).catch(function(errors) {
+        if(that.printErrors)
+          console.error(errors);
+      })
+    })
+  }
+
+  const setupAspectJoinpoint = function (joinpoint) {
+    try {
+      const event = joinpoint.args[0];
+      const context = joinpoint.args[1];
+
+      // send off the input event
+      const logIncoming = that.logIncoming(event, context);
+
+      // setup our aspects
+      setupContextAspect(context, event, logIncoming);
+    } catch (ex) {
+      if (that.printErrors) {
+        console.error(ex);
+      }
+    }
+    joinpoint.proceed();
+  };
+
+
+  that.handler = function(handler) {
+    return meld.around(handler, setupAspectJoinpoint);
+  };
+
 }
 
-module.exports = DashBotAmazonAlexa
+module.exports = DashBotAmazonAlexa;
