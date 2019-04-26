392fdfa5a8402ca833e6156b81c1ea4f3a156ebd
Dennis Yang <sinned@gmail.com>
2019-04-25 21:08:28 -0700

    removing conversationId from the npm examples

--- examples/universal-example.js
+++ examples/universal-example.js
@@ -1,13 +1,17 @@
-'use strict';
+"use strict";
 
 if (!process.env.DASHBOT_API_KEY_GENERIC) {
-  throw new Error('"DASHBOT_API_KEY_GENERIC" environment variable must be defined');
+  throw new Error(
+    '"DASHBOT_API_KEY_GENERIC" environment variable must be defined'
+  );
 }
 
-const dashbot = require('../src/dashbot')(process.env.DASHBOT_API_KEY_GENERIC,
-  {debug:true, urlRoot: process.env.DASHBOT_URL_ROOT}).universal
+const dashbot = require("../src/dashbot")(process.env.DASHBOT_API_KEY_GENERIC, {
+  debug: true,
+  urlRoot: process.env.DASHBOT_URL_ROOT
+}).universal;
 
-var readline = require('readline');
+var readline = require("readline");
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -15,31 +19,41 @@ var rl = readline.createInterface({
 
 function ask(question) {
   const messageForDashbot = {
-    "text": question,
-    "userId": "USERIDHERE123123",
-    "conversationId": "GROUPCHATID234",
-    "platformJson": {
-      "whateverJson": "any JSON specific to your platform can be stored here"
+    text: question,
+    userId: "USERIDHERE123123",
+    platformUserJson: {
+      first_name: "Bob",
+      last_name: "Loblaw",
+      gender: "male",
+      locale: "en_US"
+    },
+    platformJson: {
+      whateverJson: "any JSON specific to your platform can be stored here"
     }
   };
   dashbot.logOutgoing(messageForDashbot);
 
   rl.question(question, function(answer) {
     const messageForDashbot = {
-      "text": answer,
-      "userId": "USERIDHERE123123",
-      "conversationId": "GROUPCHATID234",
-      "platformJson": {
-        "whateverJson": "any JSON specific to your platform can be stored here"
+      text: answer,
+      userId: "USERIDHERE123123",
+      platformUserJson: {
+        first_name: "Bob",
+        last_name: "Loblaw",
+        gender: "male",
+        locale: "en_US"
+      },
+      platformJson: {
+        whateverJson: "any JSON specific to your platform can be stored here"
       }
     };
     dashbot.logIncoming(messageForDashbot);
-    if (answer === 'quit') {
+    if (answer === "quit") {
       rl.close();
       return;
     }
-    ask('You are right when you say: ' + answer + '. What else? ');
+    ask("You are right when you say: " + answer + ". What else? ");
   });
 }
 
-ask('Tell me your thoughts: ');
+ask("Tell me your thoughts: ");
